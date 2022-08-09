6096fcce9e48f818408ce15f3c5564956de593e5
Fuba <kotingfunk@gmail.com>
2022-08-09 10:10:54 -0400

    category-routes.js: complete CRUD routes operations for post and delete

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -12,10 +12,10 @@ router.get('/', async (req, res) => {
       include: [{model:Product}] 
     });
   
-    res.json(categories);
+    res.status(200).json(categories);
   }
   catch(err){
-    res.json(err);
+    res.status(500).json(err);
   }
   
 });
@@ -32,23 +32,66 @@ router.get('/:id', async (req, res) => {
       }]
     });
 
-    res.json(categoryData);
+    if(!categoryData) {
+      res.status(404).json({ message: 'No category found with this id!'});
+    }
+
+    res.status(200).json(categoryData);
   }
   catch(err) {
-    res.json(err);
+    res.status(500).json(err);
   }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new category
+  try {
+    const categoryData = await Category.create(req.body);
+    res.status(200).json(categoryData)
+  }
+  catch (err) {
+    res.status(400).json(err)
+  }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a category by its `id` value
+  try {
+    const categoryData = await Category.update(req.body, {
+      where: {
+        id: req.params.id
+      }
+    });
+
+    if(!categoryData) {
+      res.status(404).json({ message: 'No category found with this id!'});
+    }
+
+    res.status(200).json(categoryData);
+  }
+  catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
+  try {
+    const categoryData = await Category.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+
+    if(!categoryData) {
+      res.status(404).json({ message: 'No category found with this id!'});
+    }
+
+    res.json(categoryData);
+  }
+  catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
